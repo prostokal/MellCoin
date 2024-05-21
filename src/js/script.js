73956b25fad82b49847ddc602bcb287b0f13fa33
Nikita <crysisov@gmail.com>
2024-05-21 15:49:25 +0300

    1.1|version-'Tokenomica'

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+
+//MellCoin3d
+const mv = document.querySelector(".promo-model");
+mv.addEventListener("wheel", (e) => {e.stopPropagation();}, true);
+//Tokenomica
+const tokPercent = document.querySelectorAll('.tokenomika__item-percent');
+const tokLines = document.querySelectorAll('.tokenomika__item-slider span');
+tokPercent.forEach( (item, i)=> {
+    tokLines[i].style.width = item.innerHTML;
+});
