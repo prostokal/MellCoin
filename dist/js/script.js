50e81b4675d4b9ea673d7b2f5e12665831e1106f
Nikita <crysisov@gmail.com>
2024-05-31 16:35:03 +0300

    end

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,3 +5,15 @@ const tokLines = document.querySelectorAll('.tokenomika__item-slider span');
 tokPercent.forEach((item, i) => {
     tokLines[i].style.width = item.innerHTML;
 });
+// Hamburger
+
+const hamburger = document.querySelector('.hamburger')
+const menuClose = document.querySelector('.menu__close')
+const menu = document.querySelector('.menu')
+
+hamburger.addEventListener('click', (e) => {
+    menu.classList.add('active')
+});
+menuClose.addEventListener('click', (e) => {
+    menu.classList.remove('active')
+})
